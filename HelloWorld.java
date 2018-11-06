91ef0a12be1ffef2cd642a994e10819cafd4f1cc
wilder <marthe.charpy@gmail.com>
2018-11-06 10:39:05 +0100

    atelier Hello World

---
+++ HelloWorld.java
@@ -0,0 +1,13 @@
+class HelloWorld{
+
+	public static void main(String[] args) {
+
+		int hours = 8;
+		if (hours>=6 && hours<=20) {
+			System.out.println("Hello World");
+		}
+		else{
+		System.out.println("Good Night");
+		}
+	}
+}
